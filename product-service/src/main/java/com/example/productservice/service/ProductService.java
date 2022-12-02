f315c15b882db1f1a5325d5a8966f622d183a7b2
TannerLankford1223 <T.Lankford730@gmail.com>
2022-12-02 16:26:51 -0600

    Implement methods to pass unit tests

--- product-service/src/main/java/com/example/productservice/service/ProductService.java
+++ product-service/src/main/java/com/example/productservice/service/ProductService.java
@@ -4,23 +4,24 @@ import com.example.productservice.dto.NewProductRequest;
 import com.example.productservice.model.Category;
 import com.example.productservice.model.Product;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 import java.util.List;
 
 public interface ProductService {
-    Page<Product> getAllProducts();
+    Page<Product> getAllProducts(Pageable pageable);
 
-    Page<Product> getAllProductsWithNameContaining(String productName);
+    Page<Product> getAllProductsWithNameContaining(String productName, Pageable pageable);
 
-    Page<Product> getAllProductsByCategory(Category category);
+    Page<Product> getAllProductsByCategory(Category category, Pageable pageable);
 
     Product findProductById(String productId);
 
     Product addProduct(NewProductRequest newProductRequest);
 
-    void increaseInventory(String productId);
+    void increaseInventory(String productId, String size);
 
-    void decreaseInventory(String productId);
+    void decreaseInventory(String productId, String size);
 
     void deleteProduct(String productId);
 
--- product-service/src/main/java/com/example/productservice/service/ProductServiceImpl.java
+++ product-service/src/main/java/com/example/productservice/service/ProductServiceImpl.java
@@ -1,62 +1,117 @@
 package com.example.productservice.service;
 
 import com.example.productservice.dto.NewProductRequest;
+import com.example.productservice.exception.InsufficientInventoryException;
+import com.example.productservice.exception.InvalidIdException;
+import com.example.productservice.exception.InvalidSizeException;
 import com.example.productservice.model.Category;
 import com.example.productservice.model.Product;
 import com.example.productservice.persistence.ProductRepository;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 @Service
 @AllArgsConstructor
+@Slf4j
 public class ProductServiceImpl implements ProductService {
     private final ProductRepository productRepo;
 
     @Override
-    public Page<Product> getAllProducts() {
-        return null;
+    public Page<Product> getAllProducts(Pageable pageable) {
+        return productRepo.findAll(pageable);
     }
 
     @Override
-    public Page<Product> getAllProductsWithNameContaining(String productName) {
-        return null;
+    public Page<Product> getAllProductsWithNameContaining(String productName, Pageable pageable) {
+        return productRepo.findByProductNameContaining(productName, pageable);
     }
 
     @Override
-    public Page<Product> getAllProductsByCategory(Category category) {
-        return null;
+    public Page<Product> getAllProductsByCategory(Category category, Pageable pageable) {
+        return productRepo.findByCategory(category, pageable);
     }
 
     @Override
     public Product findProductById(String productId) {
-        return null;
+        Optional<Product> productOpt = productRepo.findById(productId);
+
+        if (productOpt.isEmpty()) {
+            throw new InvalidIdException(productId);
+        }
+
+        return productOpt.get();
     }
 
     @Override
     public Product addProduct(NewProductRequest newProductRequest) {
-        return null;
+        Product newProduct = new Product(newProductRequest.getProductName(),
+                newProductRequest.getPrice(),
+                newProductRequest.getCategory(),
+                newProductRequest.getStatus(),
+                newProductRequest.getSizes());
+
+        return productRepo.save(newProduct);
     }
 
     @Override
-    public void increaseInventory(String productId) {
+    public void increaseInventory(String productId, String size) {
+        Optional<Product> productOpt = productRepo.findById(productId);
 
+        if (productOpt.isEmpty()) {
+            throw new InvalidIdException(productId);
+        }
+
+        Product product = productOpt.get();
+        if (!product.getSizes().containsKey(size)) {
+            throw new InvalidSizeException(productId, size);
+        }
+
+        int initialSize = product.getSizes().get(size);
+        product.getSizes().put(size, initialSize + 1);
+
+        productRepo.save(product);
     }
 
     @Override
-    public void decreaseInventory(String productId) {
+    public void decreaseInventory(String productId, String size) {
+        Optional<Product> productOpt = productRepo.findById(productId);
 
+        if (productOpt.isEmpty()) {
+            throw new InvalidIdException(productId);
+        }
+
+        Product product = productOpt.get();
+        if (!product.getSizes().containsKey(size)) {
+            throw new InvalidSizeException(productId, size);
+        }
+
+        if (product.getSizes().get(size) <= 0) {
+            throw new InsufficientInventoryException(productId, size);
+        }
+
+        int initialSize = product.getSizes().get(size);
+        product.getSizes().put(size, initialSize - 1);
+
+        productRepo.save(product);
     }
 
     @Override
     public void deleteProduct(String productId) {
-
+        productRepo.deleteById(productId);
     }
 
     @Override
     public List<String> getAllCategories() {
-        return null;
+        return Stream.of(Category.values())
+                .map(Category::name)
+                .collect(Collectors.toList());
     }
 }
