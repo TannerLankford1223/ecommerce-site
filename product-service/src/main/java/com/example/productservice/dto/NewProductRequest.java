ac3bf44212efc759812c9ee7931eeb65eea6038e
TannerLankford1223 <T.Lankford730@gmail.com>
2022-12-02 16:24:50 -0600

    Implement a new product dto

--- product-service/src/main/java/com/example/productservice/dto/NewProductRequest.java
+++ product-service/src/main/java/com/example/productservice/dto/NewProductRequest.java
@@ -1,4 +1,23 @@
 package com.example.productservice.dto;
 
+import com.example.productservice.model.Category;
+import com.example.productservice.model.Status;
+import lombok.Builder;
+import lombok.Data;
+
+import java.math.BigDecimal;
+import java.util.Map;
+
+@Data
+@Builder
 public class NewProductRequest {
+    private String productName;
+
+    private BigDecimal price;
+
+    private Category category;
+
+    private Status status;
+
+    private Map<String, Integer> sizes;
 }
