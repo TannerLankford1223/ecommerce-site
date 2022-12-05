07c908a65a9d1824efe222b0dd9b99205d2558e8
TannerLankford1223 <T.Lankford730@gmail.com>
2022-12-05 13:17:27 -0600

    Refactor: move add/delete methods to the admin  service

--- product-service/src/main/java/com/example/productservice/controller/ProductController.java
+++ product-service/src/main/java/com/example/productservice/controller/ProductController.java
@@ -1,15 +1,15 @@
 package com.example.productservice.controller;
 
-import com.example.productservice.dto.NewProductRequest;
+import com.example.productservice.hateoas.ProductModelAssembler;
 import com.example.productservice.model.Category;
 import com.example.productservice.model.Product;
 import com.example.productservice.model.ProductModel;
-import com.example.productservice.model.ProductModelAssembler;
 import com.example.productservice.service.ProductService;
 import lombok.AllArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.web.PagedResourcesAssembler;
 import org.springframework.hateoas.PagedModel;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -19,7 +19,7 @@ import java.util.List;
 @RequestMapping("/api/v1/products")
 @AllArgsConstructor
 public class ProductController {
-    private final ProductService productRepo;
+    private final ProductService productService;
 
     private final ProductModelAssembler productModelAssembler;
 
@@ -28,7 +28,9 @@ public class ProductController {
     @GetMapping("/")
     public PagedModel<ProductModel> getAllProducts(@RequestParam(defaultValue = "0") int page,
                                                    @RequestParam(defaultValue = "10") int size) {
-        return null;
+        Page<Product> productPage = productService.getAllProducts(PageRequest.of(page, size));
+
+        return pagedResourcesAssembler.toModel(productPage, productModelAssembler);
     }
 
     @GetMapping("/findByNameContaining")
@@ -47,18 +49,6 @@ public class ProductController {
 
     @GetMapping("/{productId}")
     public ProductModel getProduct(@PathVariable("productId") long productId) {
-        return null;
-    }
-
-    @PostMapping("/secure/new")
-    public ResponseEntity<String> addNewProduct(@RequestHeader(value = "Authorization") String token,
-                                                @RequestBody NewProductRequest newProductRequest) {
-        return null;
-    }
-
-    @DeleteMapping("/secure/{productId}/delete")
-    public ResponseEntity<String> deleteProduct(@RequestHeader(value = "Authorization") String token,
-                                                @PathVariable("productId") String productId) {
         return null;
     }
 
--- product-service/src/main/java/com/example/productservice/hateoas/ProductModelAssembler.java
+++ product-service/src/main/java/com/example/productservice/hateoas/ProductModelAssembler.java
@@ -1,9 +1,14 @@
-package com.example.productservice.model;
+package com.example.productservice.hateoas;
 
 import com.example.productservice.controller.ProductController;
+import com.example.productservice.model.Product;
+import com.example.productservice.model.ProductModel;
 import org.springframework.beans.BeanUtils;
+import org.springframework.hateoas.Link;
 import org.springframework.hateoas.server.mvc.RepresentationModelAssemblerSupport;
 import org.springframework.stereotype.Component;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
 
 /**
  * The ProductModelAssembler class extends the HATEOAS Representation Model and is required for pagination.
@@ -22,6 +27,8 @@ public class ProductModelAssembler extends RepresentationModelAssemblerSupport<P
     public ProductModel toModel(Product entity) {
         ProductModel model = new ProductModel();
         BeanUtils.copyProperties(entity, model);
+        Link selfLink = linkTo(ProductController.class).slash(entity.getId()).withSelfRel();
+        model.add(selfLink);
         return model;
     }
 }
