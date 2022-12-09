4167ec5d36300d1b9165776f740b2d0e7e2eccda
TannerLankford1223 <T.Lankford730@gmail.com>
2022-12-09 17:26:04 -0600

    Bug Fix: duplicate dependency collisions

--- product-service/src/main/java/com/example/productservice/config/SwaggerConfig.java
+++ product-service/src/main/java/com/example/productservice/config/SwaggerConfig.java
@@ -2,7 +2,11 @@ package com.example.productservice.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.hateoas.client.LinkDiscoverer;
+import org.springframework.hateoas.client.LinkDiscoverers;
+import org.springframework.hateoas.mediatype.collectionjson.CollectionJsonLinkDiscoverer;
 import org.springframework.http.ResponseEntity;
+import org.springframework.plugin.core.SimplePluginRegistry;
 import org.springframework.web.bind.annotation.RestController;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
@@ -11,7 +15,9 @@ import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 @Configuration
@@ -42,5 +48,12 @@ public class SwaggerConfig {
                 "License for API",
                 "API Licence URL");
         return apiInfo;
+    }
+
+    @Bean
+    public LinkDiscoverers discoverers() {
+        List<LinkDiscoverer> plugins = new ArrayList<>();
+        plugins.add(new CollectionJsonLinkDiscoverer());
+        return new LinkDiscoverers(SimplePluginRegistry.create(plugins));
     }
 }
