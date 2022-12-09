6ba40e839c67d803ed31207ddd8e24a8b2756d8c
TannerLankford1223 <T.Lankford730@gmail.com>
2022-12-09 17:45:36 -0600

    Implement methods

--- product-service/src/main/java/com/example/productservice/service/CategoryServiceImpl.java
+++ product-service/src/main/java/com/example/productservice/service/CategoryServiceImpl.java
@@ -1,5 +1,7 @@
 package com.example.productservice.service;
 
+import com.example.productservice.exception.InvalidIdException;
+import com.example.productservice.model.Category;
 import com.example.productservice.persistence.CategoryRepository;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -13,11 +15,22 @@ public class CategoryServiceImpl implements CategoryService {
 
     @Override
     public void addCategory(String categoryName) {
+        Category newCategory = new Category(categoryName);
+        categoryRepo.save(newCategory);
 
+        log.info("New Category: " + categoryName + " created.");
     }
 
     @Override
     public void deleteCategory(long categoryId) {
+        boolean categoryExists = categoryRepo.existsById(categoryId);
 
+        if (!categoryExists) {
+            throw new InvalidIdException(categoryId);
+        }
+
+        categoryRepo.deleteById(categoryId);
+
+        log.info("Category with ID: " + categoryId + " deleted.");
     }
 }
