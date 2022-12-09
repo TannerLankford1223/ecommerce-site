1db35ac3822103d1b9ae6d90f0f860ccb1040e5d
TannerLankford1223 <T.Lankford730@gmail.com>
2022-12-09 17:26:38 -0600

    Refactor: make the category variable a String

---
+++ product-service/src/main/java/com/example/productservice/dto/NewProductRequest.java
@@ -0,0 +1,19 @@
+package com.example.productservice.dto;
+
+import lombok.Builder;
+import lombok.Data;
+
+import java.math.BigDecimal;
+import java.util.Map;
+
+@Data
+@Builder
+public class NewProductRequest {
+    private String productName;
+
+    private BigDecimal price;
+
+    private String category;
+
+    private Map<String, Integer> sizes;
+}
