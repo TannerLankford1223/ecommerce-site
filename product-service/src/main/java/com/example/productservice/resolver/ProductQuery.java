264c031dad4b6d829289259502cc513f3d4f2cd1
TannerLankford1223 <T.Lankford730@gmail.com>
2022-12-15 15:08:00 -0600

    Add method for retrieving all products

--- product-service/src/main/java/com/example/productservice/resolver/ProductQuery.java
+++ product-service/src/main/java/com/example/productservice/resolver/ProductQuery.java
@@ -1,10 +1,12 @@
 package com.example.productservice.resolver;
 
+import com.example.productservice.dto.SearchRequest;
 import com.example.productservice.model.Product;
 import com.example.productservice.service.ProductService;
 import graphql.kickstart.tools.GraphQLQueryResolver;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Component;
 
 @Slf4j
@@ -12,6 +14,11 @@ import org.springframework.stereotype.Component;
 @AllArgsConstructor
 public class ProductQuery implements GraphQLQueryResolver {
     private final ProductService productService;
+
+    public Page<Product> allProducts(String searchTerm, String category, int page, int size) {
+        SearchRequest request = new SearchRequest(searchTerm, category, page, size);
+        return productService.getProducts(request);
+    }
 
     public Product productById(long productId) {
         log.info("Retrieving product id: {}", productId);
