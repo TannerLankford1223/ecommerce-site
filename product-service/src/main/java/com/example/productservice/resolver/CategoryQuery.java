d0362f2ca338bf0fb6df6435dcdefb8c782fd033
TannerLankford1223 <T.Lankford730@gmail.com>
2022-12-15 14:27:35 -0600

    Create resolver for category queries

---
+++ product-service/src/main/java/com/example/productservice/resolver/CategoryQuery.java
@@ -0,0 +1,22 @@
+package com.example.productservice.resolver;
+
+import com.example.productservice.model.Category;
+import com.example.productservice.service.CategoryService;
+import graphql.kickstart.tools.GraphQLQueryResolver;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+@Slf4j
+@AllArgsConstructor
+public class CategoryQuery implements GraphQLQueryResolver {
+    private final CategoryService categoryService;
+
+    public List<Category> allCategories() {
+        log.info("Retrieving categories");
+        return categoryService.getAllCategories();
+    }
+}
