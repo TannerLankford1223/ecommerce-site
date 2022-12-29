569a629ec370df64dfb4b2335e38f94ab5f7fb24
TannerLankford1223 <T.Lankford730@gmail.com>
2022-12-28 22:32:14 -0600

    Refactor: Move util function to a separate file

--- product-service/src/test/java/com/example/productservice/graphql/query/ProductQueryIntegrationTests.java
+++ product-service/src/test/java/com/example/productservice/graphql/query/ProductQueryIntegrationTests.java
@@ -3,19 +3,17 @@ package com.example.productservice.graphql.query;
 import com.example.productservice.ProductServiceApplication;
 import com.graphql.spring.boot.test.GraphQLResponse;
 import com.graphql.spring.boot.test.GraphQLTestTemplate;
-import io.micrometer.core.instrument.util.IOUtils;
 import org.json.JSONException;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.core.io.ClassPathResource;
 import org.springframework.http.HttpStatus;
 import org.springframework.test.context.ActiveProfiles;
 
 import java.io.IOException;
-import java.nio.charset.StandardCharsets;
 
+import static com.example.productservice.util.JsonParsing.readResponseFile;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.skyscreamer.jsonassert.JSONAssert.assertEquals;
 
@@ -89,9 +87,4 @@ public class ProductQueryIntegrationTests {
         assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
         assertEquals(expectedresponseBody, response.getRawResponse().getBody(), true);
     }
-
-    private String readResponseFile(String location) throws IOException {
-        return IOUtils.toString(new ClassPathResource(location).getInputStream(), StandardCharsets.UTF_8);
-    }
-
 }
