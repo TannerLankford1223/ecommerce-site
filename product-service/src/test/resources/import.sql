a5867e70c4b58c3103efc428e61e843615835524
TannerLankford1223 <T.Lankford730@gmail.com>
2023-01-10 19:06:52 -0600

    Bug Fix: ID's clash with db insertion in tests

--- product-service/src/test/resources/import.sql
+++ product-service/src/test/resources/import.sql
@@ -1,13 +1,13 @@
-INSERT INTO category VALUES (1, 'Shirts');
-INSERT INTO category VALUES (2, 'Pants');
-INSERT INTO category VALUES (3, 'Jackets');
+INSERT INTO category VALUES (10, 'Shirts');
+INSERT INTO category VALUES (11, 'Pants');
+INSERT INTO category VALUES (12, 'Jackets');
 
-INSERT INTO product VALUES (1,'A grey shirt', 25.00, 'Grey Shirt', 1);
-INSERT INTO product VALUES (2, 'A blue pair of pants', 35.00, 'Blue Pants', 2);
-INSERT INTO product VALUES (3, 'A red jacket', 45.00, 'Red Jacket', 3);
-INSERT INTO product VALUES (4, 'A blue shirt',  23.99, 'Blue Shirt', 1);
-INSERT INTO product VALUES (5, 'A grey pair of pants', 33.00, 'Grey Pants', 2);
-INSERT INTO product VALUES (6, 'A blue jacket', 47.00, 'Blue Jacket', 3);
-INSERT INTO product VALUES (7, 'A black shirt', 30.50, 'Black Shirt', 1);
-INSERT INTO product VALUES (8, 'A black pair of pants', 31.99, 'Black Pants', 2);
-INSERT INTO product VALUES (9,  'A green jacket', 43.99, 'Green Jacket', 3);
+INSERT INTO product VALUES (5,'A grey shirt', 25.00, 'Grey Shirt', 10);
+INSERT INTO product VALUES (6, 'A blue pair of pants', 35.00, 'Blue Pants', 11);
+INSERT INTO product VALUES (7, 'A red jacket', 45.00, 'Red Jacket', 12);
+INSERT INTO product VALUES (8, 'A blue shirt',  23.99, 'Blue Shirt', 10);
+INSERT INTO product VALUES (9, 'A grey pair of pants', 33.00, 'Grey Pants', 11);
+INSERT INTO product VALUES (10, 'A blue jacket', 47.00, 'Blue Jacket', 12);
+INSERT INTO product VALUES (11, 'A black shirt', 30.50, 'Black Shirt', 10);
+INSERT INTO product VALUES (12, 'A black pair of pants', 31.99, 'Black Pants', 11);
+INSERT INTO product VALUES (13,  'A green jacket', 43.99, 'Green Jacket', 12);
