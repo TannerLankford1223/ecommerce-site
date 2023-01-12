122cef406014a631b34661f7eead2fbad1606546
TannerLankford1223 <T.Lankford730@gmail.com>
2023-01-12 12:53:38 -0600

    Add query for finding Product by name

--- product-service/src/main/java/com/example/productservice/persistence/ProductRepository.java
+++ product-service/src/main/java/com/example/productservice/persistence/ProductRepository.java
@@ -7,8 +7,13 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
+
+    Optional<Product> findByProductName(String productName);
+
     Page<Product> findByProductNameContaining(String productName, Pageable pageable);
 
     Page<Product> findProductsByProductNameContainingAndCategory(String productName, Category category, Pageable pageable);
